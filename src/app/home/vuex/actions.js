7c0bcbe1d2c406c6e7dfad1eac979172ad77d4dd
Pásztor Gábor <gpasztor87@gmail.com>
2017-11-26 18:38:47 +0100

    Remove unused dispatch from gym actions.

--- src/app/home/vuex/actions.js
+++ src/app/home/vuex/actions.js
@@ -1,19 +1,19 @@
 import axios from 'axios'
 import { API_URL } from '../../constants'
 
-export const getGyms = ({ dispatch, commit }) => {
+export const getGyms = ({ commit }) => {
   axios.get(API_URL + '/gyms').then((response) => {
     commit('setGyms', response.data.data)
   })
 }
 
-export const getGym = ({ dispatch, commit, state }, id) => {
+export const getGym = ({ commit, state }, id) => {
   return axios.get(API_URL + '/gyms/' + id).then((response) => {
     commit('setGym', response.data.data)
   })
 }
 
-export const createGym = ({ dispatch, commit }, { payload }) => {
+export const createGym = ({ commit }, { payload }) => {
   return axios.post(API_URL + '/gyms/create', payload).then((response) => {
     commit('createGym', response.data.data)
   }).catch((error) => {
