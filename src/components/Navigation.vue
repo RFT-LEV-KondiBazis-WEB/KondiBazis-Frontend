86397df0fb9bb76398a2459d483dc55e59cb7385
Pásztor Gábor <gpasztor87@gmail.com>
2017-12-10 11:43:59 +0100

    Install vue2-filters package. Remove passes from navigation.

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -16,9 +16,6 @@
             <router-link :to="{ name: 'customers' }" class="link-plain link-muted text-medium mr-7">
               <svg xmlns="http://www.w3.org/2000/svg" class="icon text-light-muted mr-2" viewBox="0 0 20 20"><path d="M7 8a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0 1c2.15 0 4.2.4 6.1 1.09L12 16h-1.25L10 20H4l-.75-4H2L.9 10.09A17.93 17.93 0 0 1 7 9zm8.31.17c1.32.18 2.59.48 3.8.92L18 16h-1.25L16 20h-3.96l.37-2h1.25l1.65-8.83zM13 0a4 4 0 1 1-1.33 7.76 5.96 5.96 0 0 0 0-7.52C12.1.1 12.53 0 13 0z"/></svg> Customers
             </router-link>
-            <router-link :to="{ name: 'passes' }" class="link-plain link-muted text-medium mr-7">
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon text-light-muted mr-2" viewBox="0 0 20 20"><path d="M20 12v5H0v-5a2 2 0 1 0 0-4V3h20v5a2 2 0 1 0 0 4zM3 5v10h14V5H3zm7 7.08l-2.92 2.04L8.1 10.7 5.27 8.56l3.56-.08L10 5.12l1.17 3.36 3.56.08-2.84 2.15 1.03 3.4L10 12.09z"/></svg> Passes
-            </router-link>
           </nav>
 
           <div>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vue2Filters from 'vue2-filters'
 import router from './router'
 import store from './vuex'
 import localforage from 'localforage'
@@ -9,6 +10,8 @@ localforage.config({
   driver: localforage.LOCALSTORAGE,
   storeName: 'fitbase'
 })
+
+Vue.use(Vue2Filters)
 
 Vue.config.productionTip = false
 
